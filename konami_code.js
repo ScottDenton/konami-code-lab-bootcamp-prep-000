2d9dbe536a5562f94d08960fa18e96dd3f247983
scottyd_14@hotmail.com <scottyd_14@hotmail.com>
2018-09-13 17:02:32 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,32 +1,32 @@
 const codes = [
-  //"ArrowUp",
-  38,
-  //"ArrowUp",
-  38,
-  //"ArrowDown",
-  40,
-  //"ArrowDown",
-  40,
-  //"ArrowLeft",
-  37,
-  //"ArrowRight",
-  39,
-  //"ArrowLeft",
-  37,
-  //"ArrowRight",
-  39,
-  //"b",
-  66,
-  //"a"
-  65
+  "ArrowUp",
+  
+  "ArrowUp",
+  
+  "ArrowDown",
+  
+  "ArrowDown",
+  
+  "ArrowLeft",
+  
+  "ArrowRight",
+  
+  "ArrowLeft",
+  
+  "ArrowRight",
+  
+  "b",
+  
+  "a"
+  
 ];
 
   function init(){
 var index = 0;
   
 function konami(event){
-    var key = event.which;
-    
+    var key = event.key;
+    debugger
     if (key === codes[index]){
       index ++;
       
